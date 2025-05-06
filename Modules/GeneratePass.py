68f69a7215dbad49193eebc999d027724c0deb00
NolanWong <nolancwong@gmail.com>
2025-05-06 09:53:06 -0500

    Added PassWord Generator

--- Modules/GeneratePass.py
+++ Modules/GeneratePass.py
@@ -1,5 +1,33 @@
+import random as rand
 
 class Generate:
     def __init__(self):
-        return "Password123!"
+        passw=[]
+        cap=False
+        spec=False
+        num=False
+        # return "Password123!"
+        while len(passw) <= 8:
+            if len(passw) >=5:
+                if spec==False:
+                    passw.append(chr(rand.randint(35,38)))
+                if cap==False:
+                    passw.append(chr(rand.randint(65,90)))
+                if num==False:
+                    passw.append(str(rand.randint(0,9)))
+            selector=rand.randint(1,4)
+            if selector==1:
+                passw.append(str(rand.randint(0,9)))
+                num=True
+            elif selector==2:
+                passw.append(chr(rand.randint(65,90)))
+                cap=True
+            elif selector==3:
+                passw.append(chr(rand.randint(97,122)))
+            elif selector==4:
+                passw.append(chr(rand.randint(35,38)))
+                spec=True
+        return "".join(passw)
+                
+                    
     #TODO: Make it actually return a generated password
